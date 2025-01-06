cff5e7b7f1abbdd38a094f916b06e9b591418d7f
unknown <sandeepsahu99775566@gmail.com>
2025-01-07 00:56:35 +0530

    second

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <section className="f-wrapper">
+        <div className="paddings innerWidth flexCenter f-container">
+
+ {/* {left side} */}
+
+ <div className="flexColStart f-left">
+    <img src="./logo2.png" alt=""  width={120}/>
+
+    <span className="secondaryText">
+        Our vision is to make all people <br />
+        the best place to live for them.
+    </span>
+ </div>
+
+ <div className="flexColStart f-right">
+    <span className='primaryText'>Information</span>
+    <span className='secondaryText'>Ghaziabad, 201206 ,UP ,India</span>
+    <div className="flexCenter f-menu">
+        <span className='secondaryText'>Property</span>
+        <span className='secondaryText'>Services</span>
+        <span className='secondaryText'>Product</span>
+        <span className='secondaryText'>About Us</span>
+    </div>
+ </div>
+
+
+        </div>
+    </section>
+  )
+}
+
+export default Footer
